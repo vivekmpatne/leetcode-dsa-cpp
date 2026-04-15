dec3e60b510102b26c3374c0efd72058f741abea
Vivek Patne <vivekpatnem@gmail.com>
2026-04-16 00:01:05 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,16 @@
+// Last updated: 4/16/2026, 12:01:06 AM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        //console.log("Hello World");
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
