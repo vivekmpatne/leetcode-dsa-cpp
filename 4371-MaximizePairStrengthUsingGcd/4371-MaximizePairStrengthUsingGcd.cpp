b556523e06e7c2cfbf4bb6954edcf2e17ef098f9
Vivek Patne <vivekpatnem@gmail.com>
2026-08-03 22:12:53 +0530

    Create 4371-MaximizePairStrengthUsingGcd

---
+++ 4371-MaximizePairStrengthUsingGcd/4371-MaximizePairStrengthUsingGcd.cpp
@@ -0,0 +1,16 @@
+// Last updated: 8/3/2026, 10:12:55 PM
+class Solution {
+public:
+    long long maxPairStrength(vector<int>& nums) {
+        long long ans = INT_MIN;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                long long l = gcd(nums[i],nums[j]);
+                long long k = (1LL*nums[i]*nums[j])/(1LL*l*l);
+                ans = max(ans,k);
+            }
+        }
+        return ans;
+    }
+};
