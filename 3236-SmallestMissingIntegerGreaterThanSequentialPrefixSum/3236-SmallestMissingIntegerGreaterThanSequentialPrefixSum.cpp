8e542495d98cd5cc23f79cc1b8b69304379ad445
Vivek Patne <vivekpatnem@gmail.com>
2026-08-11 19:55:45 +0530

    Create 3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum

---
+++ 3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum/3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/11/2026, 7:55:47 PM
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> seen(nums.begin(), nums.end());
+
+        int sum = nums[0];
+        for(int i = 1; i < n; i++){
+            if( nums[i] == nums[i-1] + 1 ) {
+                sum += nums[i];
+            }else{
+                break;
+            }
+        }
+        while ( seen.count(sum) ) {
+            sum++;
+        }
+
+        return sum;
+    }
+};
