241b33a5578206d304531b3ffc13aa9563315574
Vivek Patne <vivekpatnem@gmail.com>
2026-08-22 18:09:56 +0530

    Create 3918-CheckDivisibilityByDigitSumAndProduct

---
+++ 3918-CheckDivisibilityByDigitSumAndProduct/3918-CheckDivisibilityByDigitSumAndProduct.cpp
@@ -0,0 +1,13 @@
+// Last updated: 8/22/2026, 6:09:56 PM
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int s = 0, p = 1;
+        for( int x = n; x > 0; x /= 10){
+            const int r = x%10;
+            s += r;
+            p *= r;
+        }
+        return n % (s + p) == 0;
+    }
+};
